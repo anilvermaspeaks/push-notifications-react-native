7ca8303c08adc271d1aa42f50dffd270c1731a44
anil verma <anilcs240@gmail.com>
2021-04-10 10:49:16 +0530

    initial notification setup and send forground and background notifications

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+//running notifications in foreground
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true
+    }
+  }
+})
+
+
+
 
 export default function App() {
+  //on component mount
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS).then((statusObj) => {
+      if (statusObj.status !== 'granted') {
+        return Permissions.askAsync(Permissions.NOTIFICATIONS)
+      }
+      return statusObj;
+    }).then((statusObj) => {
+      if (statusObj.status !== 'granted') {
+        return;
+      }
+    })
+  }, [])
+
+
+  const triggerNotifications = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={triggerNotifications}
+        title="Trigger Local Notifications"
+        color="#841584"
+        accessibilityLabel="Trigger Local Notifications"
+      />
+      <Text>Hey this is anil! welcome medffdfd</Text>
       <StatusBar style="auto" />
     </View>
   );
